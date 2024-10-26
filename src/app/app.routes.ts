6296ef9fb6aeb000b3e0a3d269d29f57b1c073fe
WNCRY <dimacelujkin@gmail.com>
2024-10-26 11:59:34 +0300

    create basic routes, added dark and light themes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CharacterComponent } from './pages/character/character.component';
+import { FilmComponent } from './pages/film/film.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{title : 'Home Page', path:'', pathMatch:'full', component: HomeComponent},
+	{title : 'Film Information', path:'film/:id', pathMatch:'prefix', component: FilmComponent},
+	{title : 'Character Information', path:'character/:id', pathMatch:'prefix', component: CharacterComponent},
+	{title: 'Not Found', path:'**', component: NotFoundComponent}
+];
